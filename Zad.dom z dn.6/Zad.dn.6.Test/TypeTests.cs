95149b25e85f9572f014635e3d3e777f3328aa82
Mateusz Makochon <mccoban@wp.pl>
2023-08-20 02:02:27 +0200

    Zadanie z 8 dnia.

---
+++ Zad.dom z dn.6/Zad.dn.6.Test/TypeTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Zad.dn._6.Test;
+using Employe;
+internal class TypeTests
+{
+    [Test]
+    public void TestForStringReferenceType()
+    {
+        //arrange
+        string number1 = "Piotr";
+        string number2 = "Marek";
+        //act
+
+        //assert;
+        Assert.AreNotEqual(number1, number2);
+    }
+
+    [Test]
+    public void TestForIntValueType()
+    {
+        //arrange
+        int number1 = 1;
+        int number2 = 1;
+        //act
+
+        //assert;
+        Assert.AreEqual(number1, number2);
+    }
+    [Test]
+    public void TestForFloatValueType()
+    {
+        //arrange
+        float number1 = 1;
+        float number2 = 1;
+        //act
+
+        //assert;
+        Assert.AreEqual(number1, number2);
+    }
+    [Test]
+    public void TestForEmployeeReferenceType()
+    {
+        //arrange
+        var Employer1 = new Employee("Czesław", "Martyniuk", 56);
+        var Employer2 = new Employee("Zenon", "Kowalski", 30);
+        //act
+
+        //assert;
+        Assert.AreNotEqual(Employer1, Employer2);
+    }
+    private Employee GetEmploye(string name, string surname, int age)
+    {
+        return new Employee(name, surname, age);
+    }
+}
